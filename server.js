dca3c64ccd6e678a653bd5de3a4a44ec237d80b5
fhoinaski <sukinodoncai@gmail.com>
2023-10-20 16:59:34 -0300

    incluido o cors

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ class Server {
 
   async middlewares(app) {
     app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, uiConfig));
-    app.use(cors());
+    app.use(cors(
+      {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+      }
+    ));
     app.use(express.json());
   }
 
