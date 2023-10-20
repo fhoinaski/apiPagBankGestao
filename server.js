d6724ac18dfb39df8e11541e22e65d4f64ef2e52
fhoinaski <sukinodoncai@gmail.com>
2023-10-20 17:11:00 -0300

    corrigido o cors

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ class Server {
     app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, uiConfig));
     app.use(cors(
       {
-        origin: "*",
+        origin: ["https://buscarcodigopagbank.vercel.app","http://localhots:3333"],
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         preflightContinue: false,
         optionsSuccessStatus: 204,
@@ -31,15 +31,6 @@ class Server {
     app.use(express.json());
   }
 
-  // async database() {
-  //   try {
-  //     await connect.authenticate();
-  //     console.log("Conexão estabelecida com sucesso.");
-  //   } catch (error) {
-  //     console.error("Não foi possível conectar ao banco de dados:", error);
-  //     throw error;
-  //   }
-  // }
 
   async start(app) {
     app.listen(port, () => {
